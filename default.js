9c6ca96f282d4a64dabe94a66d08b108a0965778
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-03-03 11:58:13 +0600

    default again

---
+++ default.js
@@ -0,0 +1,16 @@
+function fullName(first,last = " "){
+    const name = first + " " + last;
+    console.log(name);
+    
+}
+
+fullName("kamruzzaman")
+
+
+function multiply(num1, num2 = 1){
+    const result = num1 * num2;
+    console.log(result);
+    
+}
+multiply(32)
+
