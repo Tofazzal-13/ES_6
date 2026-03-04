2d56701e5743270b1aa81cd4aa67f96fdef3c63f
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-03-04 11:43:56 +0600

    destructure

---
+++ destructure.js
@@ -0,0 +1,19 @@
+/* const product = {
+    name: "iphone",
+    price: 799,
+    description: "a smartphone by apple"
+}
+
+const newPrice = product.price + 100;
+const phoneName = `this is ${product.name}`; */
+
+const {name, price, brand} = {
+    name: "iphone",
+    price: 799,
+    brand: "apple",
+}
+console.log(name,price);
+
+const [num1, num2, num3, num4, num5] = [23,34,22,55,33];
+
+console.log(num4);
