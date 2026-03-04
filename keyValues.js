c413780f574d328008f7c30da47a3847872438db
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-03-04 14:00:44 +0600

    keyValues

---
+++ keyValues.js
@@ -0,0 +1,12 @@
+const king = {
+    name: 'musasa',
+    age: 24,
+    kingdom: "Pride Lands"
+}
+const keys = Object.keys(king);
+const values = Object.values(king);
+const entries = Object.entries(king);
+console.log(keys);
+console.log(values);
+console.log(entries);
+
