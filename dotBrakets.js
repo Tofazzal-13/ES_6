1e7e67682e22eca6642861c64aac818c9a0c2817
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-03-04 14:15:05 +0600

    bracket notation

---
+++ dotBrakets.js
@@ -0,0 +1,22 @@
+const employee = {
+    name: "john doe",
+    1: "desk one",
+    position: "software engineer",
+    salary: 450000
+
+}
+
+// dot notation
+console.log(employee.name);
+// console.log(employee.1);
+
+// bracket notation
+
+console.log(employee[1]);
+
+const money = employee["salary"];
+
+
+
+
+
